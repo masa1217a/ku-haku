37913fb2582c624465148f0b932957767c1d3733
masa1217a <il.avril.song.m.i.wlcaj@gmail.com>
2017-10-16 11:42:48 +0900

    param_init()の追記

--- setting.c
+++ setting.c
@@ -5,6 +5,55 @@
 #define STR_MAX 256
 #define CONFIG_FILE "settings.txt"
 
+int mot1_F;									// 脱水モーター　正転
+int mot1_R;									// 脱水モーター　逆転
+int mot1_STOP;									// 脱水モーター　停止
+int mot2_F;											// 減容モーター　正転
+int mot2_R;											// 減容モーター　逆転
+int mot2_STOP;									// 減容モーター　停止
+int MOT_Temp;										//温度
+///////////////////////////
+/* 透過型光電センサ */
+int LIGHT; 										//I2Cチェック用LED
+int PHOTO1;					//光電センサ　受光
+int PHOTO2;						//光電センサ　受光
+///////////////////////////
+/* 速度センサ */
+int SPEED1;  									//速度センサ
+int SPEED2; 									//速度センサ
+int SPEED3;  									//速度センサ
+int SPEED4;  									//速度センサ
+int GEAR_DRY;									//刀の枚数
+int GEAR_CRASH;
+int time_sp;										// 詰まり検知
+////////////////////////////
+/* 近接センサ */
+int KINSETU1; 						// 近接センサ1　投入部
+int KINSETU2;							// 近接センサ2　ドッキング部
+int KINSETU3;							// 近接センサ3　屑箱
+////////////////////////////
+/* 表示灯 */
+int GREEN;											//表示灯	緑
+int	YELLOW;										//表示灯	黃
+int	RED;												//表示灯	赤
+int BUZZER; 										//ブザー
+////////////////////////////
+/* 操作パネルボタン */
+int BUTTON1;										// スタートボタン
+int BUTTON2;									// ストップボタン
+int BUTTON3;										// 電源ボタン
+///////////////////////////
+/* 操作パネルＬＥＤ */
+int	LED1;												//LED　通常時
+int	LED2;												//LED　管理時
+////////////////////////////
+/* 管理パネル */
+int SW1;												// 脱水　電源
+int SW2;												// 脱水　正/逆
+int SW3;												// 減容　電源
+int SW4;												// 減容　正/逆
+////////////////////////////
+
 int read_param(char *param_name)
 {
     int i = 0, j = 0;
@@ -44,7 +93,38 @@ int read_param(char *param_name)
 int param_init()
 {
 
-  if(read_param("mot1_F") < 0) return -1;
+  if((mot1_F     = read_param("mot1_F")) < 0)     return -1;
+  if((mot1_R     = read_param("mot1_R")) < 0)     return -1;
+  if((mot1_STOP  = read_param("mot1_STOP")) < 0)  return -1;
+  if((mot2_F     = read_param("mot2_F")) < 0)     return -1;
+  if((mot2_R     = read_param("mot2_R")) < 0)     return -1;
+  if((mot2_STOP  = read_param("mot2_STOP")) < 0)  return -1;
+  if((LIGHT      = read_param("LIGHT")) < 0)      return -1;
+  if((PHOTO      = read_param("PHOTO")) < 0)      return -1;
+  if((PHOTO2     = read_param("PHOTO2")) < 0)     return -1;
+  if((SPEED1     = read_param("SPEED1")) < 0)     return -1;
+  if((SPEED2     = read_param("SPEED2")) < 0)     return -1;
+  if((SPEED3     = read_param("SPEED3")) < 0)     return -1;
+  if((SPEED4     = read_param("SPEED4")) < 0)     return -1;
+  if((GEAR_DRY   = read_param("GEAR_DRY")) < 0)   return -1;
+  if((GEAR_CRASH = read_param("GEAR_CRASH")) < 0) return -1;
+  if((time_sp    = read_param("time_sp")) < 0)    return -1;
+  if((KINSETU1   = read_param("KINSETU1")) < 0)   return -1;
+  if((KINSETU2   = read_param("KINSETU2")) < 0)   return -1;
+  if((KINSETU3   = read_param("KINSETU3")) < 0)   return -1;
+  if((GREEN      = read_param("GREEN")) < 0)      return -1;
+  if((RED        = read_param("RED")) < 0)        return -1;
+  if((YELLOW     = read_param("YELLOW")) < 0)     return -1;
+  if((BUZZER     = read_param("BUZZER")) < 0)     return -1;
+  if((BUTTON1    = read_param("BUTTON1")) < 0)    return -1;
+  if((BUTTON2    = read_param("BUTTON2")) < 0)    return -1;
+  if((BUTTON3    = read_param("BUTTON3")) < 0)    return -1;
+  if((LED1       = read_param("LED1")) < 0)       return -1;
+  if((LED2       = read_param("LED2")) < 0)       return -1;
+  if((SW1        = read_param("SW1")) < 0)        return -1;
+  if((SW2        = read_param("SW2")) < 0)        return -1;
+  if((SW3        = read_param("SW3")) < 0)        return -1;
+  if((SW4        = read_param("SW4")) < 0)        return -1;
 
   return 0;
 }
